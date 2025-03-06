282930201050124b9204ff49d224aa019354082c
MiguelHonda <miguelroshonda@gmail.com>
2025-03-06 19:40:53 +0100

    added XML documentation to Evento.cs - 29597425J

--- hada-p2/Eventos.cs
+++ hada-p2/Eventos.cs
@@ -6,19 +6,47 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
+    /// <summary>
+    /// Provides data for the Tocado event.
+    /// </summary>
     internal class TocadoArgs : EventArgs
     {
+        /// <summary>
+        /// The name of the object that was hit.
+        /// </summary>
         public string name;
+
+        /// <summary>
+        /// The coordinates of the impact.
+        /// </summary>
         public Coordenada coordenadaImapcato;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TocadoArgs"/> class.
+        /// </summary>
+        /// <param name="name">The name of the object that was hit.</param>
+        /// <param name="coordenadaImpacto">The coordinates of the impact.</param>
         public TocadoArgs(string name, Coordenada coordenadaImpacto)
         {
             this.name = name;
             this.coordenadaImapcato = coordenadaImpacto;
         }
     }
+
+    /// <summary>
+    /// Provides data for the Hundido event.
+    /// </summary>
     internal class HundidoArgs : EventArgs
     {
+        /// <summary>
+        /// The name of the object that was sunk.
+        /// </summary>
         public string name;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HundidoArgs"/> class.
+        /// </summary>
+        /// <param name="name">The name of the object that was sunk.</param>
         public HundidoArgs(string name)
         {
             this.name = name;
